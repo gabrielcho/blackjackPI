b5a05d08a1b2c6a35a6c283682c1698b151ef4da
gabrielcho <gabrielarango15@gmail.com>
2019-06-08 18:36:23 +0000

    Iniciando la estructuracion de todos los metodos de otras clases que va a llamar el control del juego

--- Blackjack/src/blackjack/Control.java
+++ Blackjack/src/blackjack/Control.java
@@ -1,4 +1,35 @@
 package blackjack;
+import java.util.*;
 public class Control {
+    //variables
+    boolean continuar;
+    
+    public void iniciarJuego(){
+        while( continuar == true ){ //Bucle infinito que comprueba si el jugador quiere _continuar_ para volver a ejecutarse
+            
+            //Empezamos colocando el esquema de la logica del juego
+            
+            // Saludar, pedir apuesta
+            vistaConsola.saludar(); //saluda y pide la apuesta
+            
+            //Con la apuesta ya hecha, procedemos a la parte de reparticion
+            
+            baraja.repartir(); //Reparte las cartas 2 al jugador, 2 al crupier
+            
+            //Una vez repartidas tenemos que mostrar las cartas
+            
+            vistaConsola.verMano(); //Muestra la mano de las cartas obtenidas por Jugador
+            vistaConsola.verManoCrupier(); //Muestra la unica carta del crupier
+            vistaConsola.verPuntajeJugador; //Siempre es util ver nuestro puntaje ajajjjj sisas
+            
+            //Ahora tenemos que preguntarle al jugador si con esas cartas se planta o quiere pedir más
+            
+            
+            
+            
+        }
+        
+    }
+    
+    
 }
-//yeah
