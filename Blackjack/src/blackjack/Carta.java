16e341244d0b4c0411113dc5bfcc2f2f684d859f
gabrielcho <gabrielarango15@gmail.com>
2019-06-08 02:52:32 +0000

    sisas

---
+++ Blackjack/src/blackjack/Carta.java
@@ -0,0 +1,3 @@
+package blackjack;m
+public class Carta {
+}
---
+++ Blackjack/src/blackjack/Main.java
@@ -0,0 +1,16 @@
+package blackjack;
+import java.util.*;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // write your code here
+	    System.out.println("Hello World!");
+	      List<Integer> gasPrices = new ArrayList<>();
+
+            gasPrices.add(346);
+            gasPrices.add(360);
+            gasPrices.add(354);
+    }
+}
