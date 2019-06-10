cb236b45d4f5799f5369265b1cd607625bb59b94
invitado <invitado@s2pc4.eisc.univalle.edu.co>
2019-06-10 16:23:52 -0500

    Añadidos algunos métodos a la clase Baraja

--- Blackjack/src/blackjack/Baraja.java
+++ Blackjack/src/blackjack/Baraja.java
@@ -1,3 +1,5 @@
+//Tengo que aprender a manejar mejor este tema de array de objetos puesto que buena parte de esto me tocó emularlo de otro proyecto
+
 package blackjack;
 import java.util.*;
 public class Baraja {
@@ -6,16 +8,32 @@ public class Baraja {
 	//En el constructor tenemos que crear ya la baraja
 	Carta carta = new Carta();
 	private static Carta[] baraja;
+	private int robadas;
 	public Baraja(){
-		
+		baraja[creadas] = new Carta[]
+		for(int valor = 1; valor < 15; i++){
+			for(int palo = 0; palo < 5; palo++){
+				baraja[creadas] = new Carta(valor,palo)
+				creadas++
+			}
+		}
+		robadas = 0;
 	}//
 	
 	public void barajarCartas(){ //Esta función desordena las cartas para aleatorizar la selección de la primera de la baraja
-		
+		for ( int i = 51; i > 0; i-- ) {
+			int rand = (int)(Math.random()*(i+1));
+			Carta temp = baraja[i];
+			baraja[i] = baraja[rand];
+			baraja[rand] = temp;
+			}
+			robadas = 0;
 	}//
 	
-	public Carta soltarCarta(){ // Toma la primera carta de la baraja y la arroja para que la reciba un jugador o crupier
-		
+	public Carta soltarCarta(){ // Toma la ultima carta de la baraja y la arroja para que la reciba un jugador o crupier
+		return Baraja[creadas-robadas]
+		robadas++;
+
 	}//
     
 }
